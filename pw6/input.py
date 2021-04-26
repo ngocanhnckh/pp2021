a7ff4e53eed96df9f9b0075cafa1cea21c51b462
Nguyen Ngoc Anh <anh.nguyen@youngit.org>
2021-04-26 16:44:19 +0700

    Added pickle to labwork 6

---
+++ pw6/input.py
@@ -0,0 +1,119 @@
+import domains
+import output
+from domains import student, course, mark, students, courses, marks
+import os
+import pickle
+
+from zipfile import ZipFile
+
+def readAll():
+    with open("students","rb") as sfile:
+        sts = (pickle.loads(sfile.read()))
+    for s in sts:
+        students.append(s)
+    with open("marks","rb") as mfile:
+        mks = pickle.loads(mfile.read())
+    for m in mks:
+        marks.append(m)
+    with open("courses","rb") as cfile:
+        css = pickle.loads(cfile.read()) 
+    for c in css:
+        courses.append(c)
+
+
+def addMarkFunction(sid,cid,m):
+    s = next(item for item in students if int(item.studentid) == int(sid))
+    ma = mark(sid,cid)
+    ma.setMark(m)
+    marks.append(ma)
+    #output.writeMark(ma)
+    s.gpa = output.getGPA(sid)
+            
+
+def addMark():
+    output.printStudents()
+    sID = input("Select a student by his id: ")
+    print("You selected " + str(sID))
+    for student in students:
+        #print("checking " + student.getID())
+        if (int(student.getID()) == int(sID)):
+            print("Matched!")
+            output.printCourses()
+            cID = int(input("Select a course by its id: "))
+            m = input("Enter mark:")
+            addMarkFunction(sID,cID,m)
+
+def lazyInput():
+    students.append(student("Nguyen Ngoc Anh","23/03/2001",10))
+    students.append(student("Tran Ngoc Hieu Nam","01/12/2001",54))
+    courses.append(course("Math",1,4))
+    courses.append(course("Python",2,3))
+    addMarkFunction(10,1,20)
+    addMarkFunction(10,1,15)
+    addMarkFunction(10,1,13.342)
+    addMarkFunction(10,1,8.5)
+    addMarkFunction(10,2,15)
+    addMarkFunction(10,2,20)
+    addMarkFunction(10,2,19)
+    addMarkFunction(10,2,12)
+    addMarkFunction(54,1,5)
+    addMarkFunction(54,1,15)
+    addMarkFunction(54,1,19.123)
+    addMarkFunction(54,1,12)
+    addMarkFunction(54,2,20.34)
+    addMarkFunction(54,2,20)
+    addMarkFunction(54,2,19)
+    addMarkFunction(54,2,18)
+    #output.writeStudents()
+    #output.writeCourses()
+    #output.writeMarks()
+
+def loadData():
+    with open("student.txt","r") as stxt:
+        lines = stxt.readlines()
+        for line in lines:
+            if (line):
+                a = line.split(",")
+                name = a[0]
+                studentid = a[1]
+                dob = a[2]
+                gpa= a[3]
+            students.append(student(name,dob,studentid))
+    with open("course.txt") as ctxt:
+        lines = ctxt.readlines()
+        for line in lines:
+            if (line):
+                a = line.split(",")
+                name = a[0]
+                courseid = a[1]
+                etc = a[2]
+                courses.append(course(name,courseid,etc))
+    with open("mark.txt") as mtxt:
+        lines = mtxt.readlines()
+        for line in lines:
+            if (line):
+                a = line.split(",")
+                cid = a[0]
+                sid = a[1]
+                value = a[2]
+                addMarkFunction(sid,cid,value)
+
+def InputFunction():
+    nS=int(input("Num of students: "))
+    #intput stuendts
+    for i in range (1,nS+1):
+        id = input("Enter " + str(i) + " student ID: ")
+        name = input("Student name: ")
+        dob = input("Students's date of birth: ")
+        students.append(student(name,dob,id))
+        #output.writeStudent(student(name,dob,id))
+  
+    #input courses
+    nC = int(input("Enter number of courses: "))
+    for i in range(1,nC+1):
+        id = input("Course ID: ")
+        name = input("Course name: ")
+        etc = input("How many etcs? ")
+        courses.append(course(name,id,etc))
+        #output.writeCourse(course(name,id,etc))
+    
---
+++ pw6/main.py
@@ -0,0 +1,63 @@
+import math
+import numpy 
+#import curses
+from domains import student, course, mark
+import input as inp
+import output
+import os
+from zipfile import ZipFile
+
+def main():
+    with ZipFile("students.dat","r") as zip:
+        zip.extractall()
+    option = "-1"
+    print("""
+    (                                                                                     
+    )\ )    )        (                   )                                                
+    (()/( ( /(   (    )\ )   (         ( /(      )       )            )  (  (     (   (    
+    /(_)))\()) ))\  (()/(  ))\  (     )\())    (     ( /(   (     ( /(  )\))(   ))\  )(   
+    (_)) (_))/ /((_)  ((_))/((_) )\ ) (_))/     )\  ' )(_))  )\ )  )(_))((_))\  /((_)(()\  
+    / __|| |_ (_))(   _| |(_))  _(_/( | |_    _((_)) ((_)_  _(_/( ((_)_  (()(_)(_))   ((_) 
+    \__ \|  _|| || |/ _` |/ -_)| ' \))|  _|  | '  \()/ _` || ' \))/ _` |/ _` | / -_) | '_| 
+    |___/ \__| \_,_|\__,_|\___||_||_|  \__|  |_|_|_| \__,_||_||_| \__,_|\__, | \___| |_|   
+                                                                        |___/              
+    by ngocanhnckh                                                                BI10-010
+    --------------------------------------------------------------------------------------
+    Please select an option from menu:
+    1. Input students and courese
+    2. Add mark
+    3. List current student list
+    4. List current course list
+    5. List current mark list
+    6. Calculate average score
+    7. Calculate weighted sum
+    8. Show sorted students
+    9. Exit()
+    """)
+    #inp.loadData()
+    inp.readAll()
+    
+    while (option != "9"):
+        option = input("Your option: ")
+        if (option=="1"):
+            inp.InputFunction()
+        if (option=="2"):
+            inp.addMark()
+        if (option=="3"):
+            output.printStudents()
+        if (option=="4"):
+            output.printCourses()
+        if (option=="5"):
+            output.printMark()
+        if (option=="6"):
+            output.showAvarage()
+        if (option=="7"):
+            output.showWeightedSum()
+        if (option=="8"):
+            output.sortstudentbyGPA()
+    output.writeAll()
+    output.writeZip()
+    print("Just a friendly goodbye")
+
+if __name__ == "__main__":
+    main()
